dd26bc4af886c0881b1cc0ad004d9689ecbdff01
yossi <yoshiokashu123@gmail.com>
2022-02-22 15:02:30 +0900

    first commit

---
+++ Boot-camp/Medium/35.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+
+int main() {
+    int64_t a;
+    cin >> a;
+    int c = 2 * a;
+
+
+
+    cout << c << endl;
+    cout << a << endl;
+}
+
+
---
+++ Boot-camp/Medium/36.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+
+int main() {
+    int64_t a;
+    cin >> a;
+    int b = a + 2;
+    int d = a * 2;
+
+    cout << a * a << endl;
+}
