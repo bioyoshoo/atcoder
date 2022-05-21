6647616fb2ccec7ba8277354e6f089498ac334cc
bioyoshoo <yoshiokashu123@gmail.com>
2022-05-21 20:09:02 +0900

    many 110

--- recommend/brown/arc110_b.cpp
+++ recommend/brown/arc110_b.cpp
@@ -1,16 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+using ll = long long;
+#define rep(i, n) for (int i = 0; i < (n); i++)
 
 int main() {
-    int N;
-    cin >> N;
-    string T;
-    cin >> T;
+    int N; cin >> N;
+    string T; cin >> T;
+    string S = "110";
+    ll V = 1e10;
 
-    long long ans = 0;
+    ll ans = 0;
     if (T == "1") ans = 2e10;
-    else if (T == "11") ans = 1e10;
+    else if (T == "0") ans = 1e10;
     else {
-        
+        for (int s = 0; s < 3; s++) {
+            ll a = (T.size() + s + 2) / 3;
+            ll add = V - a + 1;
+
+            bool ok = true;
+            for (int i = 0; i < N; i++) {
+                if (T[i] != S[(s + i) % 3]) ok = false;
+            }
+            if (ok) ans += add;
+        }
     }
+    cout << ans << endl;
 }
