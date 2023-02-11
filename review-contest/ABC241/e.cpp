7df98b761fbffdef0a834684fb4de818b99010d3
bioyoshoo <yoshiokashu123@gmail.com>
2023-02-11 20:17:19 +0900

    update

--- review-contest/ABC241/e.cpp
+++ review-contest/ABC241/e.cpp
@@ -10,26 +10,48 @@ int main() {
     vector<ll> A(N); rep(i, N) cin >> A[i];
 
     ll X = 0;
-    int i = 0;
-    vector<vector<pair<int, ll>>> L(N);
-    ll start = -1, T = -1, sum = -1;
+    int cnt = 0;
+    int idx = -1;
+    vector<vector<pair<int, ll>>> V(N);
+    bool circle = false;
     while (true) {
-        L[X % N].push_back(make_pair(i, X));
+        idx = X % N;
+        V[idx].push_back(make_pair(cnt, X));
 
-        if ((int)L[X % N].size() == 2) {
-            start = L[X % N][0].first;
-            T = L[X % N][1].first - L[X % N][0].first;
-            sum = L[X % N][1].second - L[X % N][0].second;
+        if ((int)V[idx].size() == 2) {
+            circle = true;
             break;
         }
 
-        i++;
-        X += A[X % N];
+        X += A[idx];
+        cnt++;
+
+        if (cnt == K) break;
+    }
+    
+    if (!circle) {
+        cout << X << endl;
+        return 0;
     }
 
-    cout << T << " " << sum << endl;
-
-    ll ans = 0;
+    ll T = V[idx][1].first - V[idx][0].first;
+    ll sum = V[idx][1].second - V[idx][0].second;
     
-    ans += 
+    X = 0;
+    for (ll i = 0; i < K;) {
+        ll j = X % N;
+
+        if (j == idx && (K - i) >= T) {
+            ll lap = (K - i) / T;
+            X += lap * sum;
+            i += lap * T;
+        }
+        else {
+            X += A[j];
+            i++;
+        }
+    }
+
+    cout << X << endl;
+    return 0;
 }
